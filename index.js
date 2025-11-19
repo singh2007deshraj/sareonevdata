f08a1c49bc217c4b93aaa2dffafeb387bfcbf907
deshraj <singh2007deshraj@gmail.com>
2025-11-19 22:44:39 +0530

    dasd

--- index.js
+++ index.js
@@ -121,7 +121,7 @@ async function processEvents(events) {
      }
      else  if (event === "userIncome") {
        let userRegId=await getuserIdnId(returnValues.receiver) || "0000";
-      const insertSql = `INSERT INTO userIncome (receiver, user_idn,sender,tokenQty,incomeAmt,incomeLevel,packageAmt,incomeType, transaction_id, block_timestamp,block_number) VALUES (?, ?, ?, ?, ?, ?, ?,?,?,?)`;
+      const insertSql = `INSERT INTO userIncome (receiver, user_idn,sender,tokenQty,incomeAmt,incomeLevel,packageAmt,incomeType, transaction_id, block_timestamp,block_number) VALUES (?, ?, ?, ?, ?, ?, ?,?,?,?,?)`;
           const values = [returnValues.receiver,userRegId,returnValues.sender,returnValues.incomeAmt ,returnValues.incomeAmt,returnValues.incomeLevel,returnValues.packageAmt,returnValues.incomeType ,transactionHash,newTimestamp,blockNumber];
           conn.query(insertSql, values, (insertErr) => {
             if (insertErr) return console.error("Insert Error:", insertErr);
